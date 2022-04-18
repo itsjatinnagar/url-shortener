0405d86944ecc9e96fea51ab92abb10a7755edf0
Jatin Nagar <itsjatinnagar@gmail.com>
2022-04-18 11:34:39 +0530

    fixed filepaths

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='assets')
 
 
 @app.route('/')
 def index():
-    return '<H1>Hello <em>World</em></H1>'
+    return render_template('index.html')
