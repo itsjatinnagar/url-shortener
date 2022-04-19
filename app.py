09d4aa0429859df298bb099980014a89bdf77bac
Jatin Nagar <itsjatinnagar@gmail.com>
2022-04-19 21:39:21 +0530

    Created: Url shortener route

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, request
+
+from model.validators import validateURL
 
 app = Flask(__name__, static_folder='assets')
 
@@ -6,3 +8,13 @@ app = Flask(__name__, static_folder='assets')
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/shorten', methods=['POST'])
+def shorten():
+    long_url = request.json['long_url']
+    validate = validateURL(long_url)
+    if validate is None:
+        return jsonify(status=200, message='OK')
+    else:
+        return jsonify(status=400, message=validate)
