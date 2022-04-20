47d2c68de2275da9d76298a64e94d17188c5de82
Jatin Nagar <itsjatinnagar@gmail.com>
2022-04-20 19:31:01 +0530

    AJAX/Fetch: post request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+const urlInput = document.getElementById("url"),
+    shortenForm = document.getElementById("url-form");
+
+// Submitting Long URL
+shortenForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const request = { long_url: urlInput.value };
+
+    fetch("/shorten", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(request),
+    })
+        .then((response) => response.json())
+        .then((data) => console.log(data.message))
+        .catch((error) => console.error("Error: " + error));
+});
+
 // Set Current Year in Copyright Footer
 const setFooter = () => {
     const element = document.getElementById("currentYear");
