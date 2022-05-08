47260a33a76257af1f9eb92ef8f93b8c623685e0
Jatin Nagar <itsjatinnagar@gmail.com>
2022-05-08 11:33:01 +0530

    Database Upgrade

--- model/initDB.py
+++ model/initDB.py
@@ -5,18 +5,27 @@ conn = connect()
 cursor = conn.cursor()
 
 # Drop (IF)Existing Tables
-cursor.execute('DROP TABLE IF EXISTS url, expired')
+cursor.execute('DROP TABLE IF EXISTS users, url, expired')
 
 # Create Tables
+cursor.execute('CREATE TABLE users('
+               'id SERIAL,'
+               'email VARCHAR(320) UNIQUE NOT NULL,'
+               'PRIMARY KEY (id))'
+               )
+
 cursor.execute('CREATE TABLE url('
+               'uid INTEGER NOT NULL,'
                'short_code CHAR(6) PRIMARY KEY,'
                'long_url VARCHAR(2000) NOT NULL,'
-               'creation CHAR(19) NOT NULL)')
+               'expiration CHAR(19) NOT NULL,'
+               'FOREIGN KEY (uid) REFERENCES users(id))'
+               )
 
 cursor.execute('CREATE TABLE expired('
                'short_code CHAR(6) PRIMARY KEY,'
-               'long_url VARCHAR(2000) NOT NULL,'
-               'expired_on CHAR(19) NOT NULL)')
+               'long_url VARCHAR(2000) NOT NULL)'
+               )
 
 conn.commit()
 
